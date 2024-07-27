ce4a9f86701dd78d650b6e9aa4a9e2e948cd0e00
Nguyen Tan <tan.nguyen@widosoft.com>
2024-07-27 11:20:10 +0700

    feat: setup profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="bg-orange-500">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="profile-card">
+        <div className="header"></div>
+        <div className="avatar"></div>
+        <h2>Johnny Rogers</h2>
+        <h4>@jonnyrogers</h4>
+        <p>
+          Crafting brand and communication strategies, creating visual designs,
+          leading art direction, and capturing portraits through photography.
+        </p>
+        <div className="social-icons">
+          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-instagram"></i>
+        </div>
+        <div className="buttons">
+          <button className="follow">Follow</button>
+          <button className="message">Message</button>
+        </div>
       </div>
     </>
   );
